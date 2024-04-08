d57b5790b1d45a17b680e7e7e47bbebdd35bd336
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2024-04-08 09:07:24 +0500

    update

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-console.log("todo app");
-export {};
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var chalk_1 = require("chalk");
+console.log(chalk_1.default.bold.bgMagentaBright("This is a CLI based to-do-list App "));
+var toDoList = [];
+var userQuestionAnswer = await inquirer_1.default.prompt([
+    {
+        name: "userQuestion",
+        type: "input",
+        message: "What would you like to add in your to-do-list?",
+    },
+]);
+toDoList.push(userQuestionAnswer.userQuestion);
+console.log(chalk_1.default.bold.bgMagentaBright(templateObject_1 || (templateObject_1 = __makeTemplateObject(["toDoList"], ["toDoList"]))));
+var templateObject_1;
--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
 import inquirer from "inquirer"
 import chalk from "chalk"
 
-console.log("To-Do-LIst App")
+console.log(chalk.bold.bgMagentaBright("This is a CLI based to-do-list App "));
+let toDoList = [];
+
+const userQuestionAnswer = await inquirer.prompt([
+
+    {
+        name: "userQuestion",
+        type: "input",
+        message:"What would you like to add in your to-do-list?"
+    }
+
+]);
+
+toDoList.push(userQuestionAnswer.userQuestion);
+
+//console.log(chalk.bold.bgMagentaBright`toDoList`);
