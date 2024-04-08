32731b44cab590f2490ce62196c522ddcbfa569a
Salahuddin Muhammad <45813964+salahsaifjalal@users.noreply.github.com>
2024-04-08 15:20:13 +0500

    update

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.bold.bgMagentaBright("This is a CLI based to-do-list App "));
 let toDoList = [];
-const userQuestionAnswer = await inquirer.prompt([
-    {
-        name: "userQuestion",
-        type: "input",
-        message: "What would you like to add in your to-do-list?"
-    }
-]);
-toDoList.push(userQuestionAnswer.userQuestion);
-//console.log(chalk.bold.bgMagentaBright("toDoList"));
+let checkCondition = true;
+while (checkCondition) {
+    const userQuestionAnswer = await inquirer.prompt([
+        {
+            name: "userQuestion",
+            type: "input",
+            message: chalk.bold.bgGreen("What would you like to add in your to-do-list?")
+        },
+        {
+            name: "userNextQuestion",
+            type: "confirm",
+            message: chalk.bold.bgBlackBright("Would you like to add more in your to-do-list?"),
+            default: "true"
+        }
+    ]);
+    toDoList.push(userQuestionAnswer.userQuestion);
+    checkCondition = userQuestionAnswer.userNextQuestion;
+}
 console.log(toDoList);
--- main.ts
+++ main.ts
@@ -3,18 +3,27 @@ import chalk from "chalk";
 
 console.log(chalk.bold.bgMagentaBright("This is a CLI based to-do-list App "));
 let toDoList = [];
+let checkCondition=true;
 
+while(checkCondition) {
 const userQuestionAnswer = await inquirer.prompt([
 
     {
         name: "userQuestion",
         type: "input",
-        message:"What would you like to add in your to-do-list?"
+        message:chalk.bold.bgGreen("What would you like to add in your to-do-list?")
+    },
+    {
+        name: "userNextQuestion",
+        type: "confirm",
+        message: chalk.bold.bgBlackBright("Would you like to add more in your to-do-list?"),
+        default: "true"
     }
 
-]);
+])
 
 toDoList.push(userQuestionAnswer.userQuestion);
+checkCondition=userQuestionAnswer.userNextQuestion
+}
 
-//console.log(chalk.bold.bgMagentaBright("toDoList"));
 console.log(toDoList)
